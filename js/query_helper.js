4c01a15b244fb695c18d72bb4b7561683032b67b
DESKTOP-7KVBV5S\ludov <autin@scripps.edu>
2018-06-25 17:03:14 -0700

    add a test when export csv

--- js/query_helper.js
+++ js/query_helper.js
@@ -1541,7 +1541,7 @@ function saveCurrentCSV(){//saveCurrentCVJSON() {
 
   for (var i = 0; i < graph.nodes.length; i++) {//graph.nodes.length
     var node = graph.nodes[i];
-    if (!node.children) {
+    if (!node.children && node.data.nodetype!=="compartment") {
       var singlerow = [];
       for (var j = 0; j < k.length; j++) {
         if ((k[j]!="visited") && (k[j]!="nodetype"))
