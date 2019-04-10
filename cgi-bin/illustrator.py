6c1991e0bd00db6f75c9710b35f56d27fd0de30b
DESKTOP-O6LIMN9\ludov <ludovic.autin@gmail.com>
2019-04-10 16:19:10 -0700

    working server

--- cgi-bin/illustrator.py
+++ cgi-bin/illustrator.py
@@ -10,7 +10,6 @@ import types
 import time
 import random
 g = random
-
 sys.path.append('../python/')
 from HTMLTools import *
 
@@ -28,16 +27,15 @@ def mkRand():
 #
 def displayForm(file = "", seq = ""):
     aStr = ""
-    aStr += htmlHeader("ABCgen",other = "<META NAME=\"keywords\" content=\"3D ABC ABCgen alphabet structure structurale alphabet\">")
-    aStr += "<div align=\"center\"><img src=\"http://bioserv.rpbs.jussieu.fr/~autin/help/ABCgen/ABCgen.png\" width=\"800\" height=\"89\"></a></div>\n"
+    aStr += htmlHeader("Illustrator",other = "<META NAME=\"keywords\" content=\"\">")
+    #aStr += "<div align=\"center\"><img src=\"http://bioserv.rpbs.jussieu.fr/~autin/help/ABCgen/ABCgen.png\" width=\"800\" height=\"89\"></a></div>\n"
     aStr += htmlRule()
-    aStr += htmlMsg("<form method=\"POST\" action=\"http://http://mgldev.scripps.edu/cgi-bin/ABCgen2\" enctype=\"multipart/form-data\"> <INPUT TYPE=HIDDEN NAME=\"key\" VALUE=\"process\">")
+    aStr += htmlMsg("<form method=\"POST\" action=\"../cgi-bin/illustrator.py\" enctype=\"multipart/form-data\"> <INPUT TYPE=HIDDEN NAME=\"key\" VALUE=\"process\">")
     aStr += "<table><tr><td>\n"
-    aStr += htmlH4Msg("This facility is to generate sentence using structural alphabet. ")
-    aStr += "<H4><font color=Blue>Enter a text&nbsp;</font><input type=\"text\"name=\"TXT\"value=\"ABC_GEN\" size=50><br><small>limited to 156 character</H4>\n"
-    aStr += "Example of uppercase Text: \"A B C_GENERATOR!\" (underscore means a new line)<br>Special characters available : . , ; : ? ! ' + - = <> () [] \\/ | <br>see the help <a href=\"http://bioserv.rpbs.jussieu.fr/~autin/help/ABCgen/help.html\" target=\"blanck\">here</a>\n"
+    aStr += htmlH4Msg("This facility is to generate protein illustration. ")
+    aStr += "<H4><font color=Blue>Enter a PDBid&nbsp;</font><input type=\"text\"name=\"TXT\"value=\"2hbb\" size=50><br><small>limited to 156 character</H4>\n"
     aStr += htmlMsg("<p> <input type=submit value=\"Process\"><input type=reset value=\"Clear\"> ")
-    aStr += "</td><td align=\"center\"><a href=\"http://bioserv.rpbs.jussieu.fr/~autin/help/ABCgen/alpha_02.png\" target=\"blanck\"><img src=\"http://bioserv.rpbs.jussieu.fr/~autin/help/ABCgen/alpha_02.png\" width=\"500\" height=\"500\"></a></div></td></tr></table>\n"
+    aStr += "</td><td align=\"center\"></div></td></tr></table>\n"
     aStr += htmlRule()
     aStr += htmlMsg("</form>")
     aStr += htmlTailer()
@@ -141,32 +139,141 @@ class processObj:
         # print "queryId: ",self.queryId
         return
 
+def prepareInput(pdbId):
+    astr="read\n"    
+    astr+=pdbId+".pdb\n"
+    astr+="""
+1.6,1.5,1.6,1.5,1.6,1.8,1.9,1.5       #radii for atom types 1-16
+1.5,1.5,1.6,1.5,1.5,1.5,1.6,1.5
+HETATM-----HOH-- 0,9999,0,0           #selection strings, dash is wildcard
+ATOM  -H-------- 0,9999,0,0           # first two numbers, residue range
+ATOM  H--------- 0,9999,0,0           #third number, atom type
+ATOM  -C-------A 5,9999,1,1           # final number defines subunits for outlines
+ATOM  ---------A 5,9999,2,1           # selection starts at first record, and stop when it finds something that matches
+ATOM  -C-------C 5,9999,1,2
+ATOM  ---------C 5,9999,2,2
+ATOM  -C-------B 5,9999,3,3
+ATOM  ---------B 5,9999,4,3
+ATOM  -C-------D 5,9999,3,4
+ATOM  ---------D 5,9999,4,4
+HETATM-C---HEM-- 0,9999,5,5
+HETATMFE---HEM-- 0,9999,7,5
+HETATM-----HEM-- 0,9999,6,5
+HETATM---------- 0,9999,0,0
+ATOM  HCCC-RES-A 0,9999,1,1
+END
+center 
+auto
+trans
+0.,0.,0.
+scale
+12.0                                                         # pixels/Angstrom
+zrot
+-90.
+xrot
+180.
+zrot
+0.
+yrot
+0.
+xrot
+0.
+wor
+-0.5,-0.2,1.2                                                #vector pointing at light source
+0.0,0.00,0,0.99, 0.0,0.00,0,0.69, 0.0,0.00,0,0.69            #phong shading parameters for each atom type
+0.0,0.00,0,0.99, 0.0,0.00,0,0.59, 0.0,0.00,0,0.59            #final numbers are flat colors, rgb
+0.0,0.00,0,0.99, 0.0,0.00,0,0.89, 0.0,0.00,0,0.69
+0.0,0.00,0,0.99, 0.0,0.00,0,0.79, 0.0,0.00,0,0.59
+0.0,0.00,0,0.99, 0.0,0.00,0,0.39, 0.0,0.00,0,0.39
+0.0,0.00,0,0.99, 0.0,0.00,0,0.29, 0.0,0.00,0,0.29
+0.0,0.00,0,0.99, 0.0,0.00,0,0.00, 0.0,0.00,0,0.00
+0.0,0.00,0,0.19, 0.0,0.00,0,0.75, 0.0,0.00,0,0.99
+0.0,0.00,0,0.99, 0.0,0.00,0,0.19, 0.0,0.00,0,0.19
+0.0,0.00,0,0.29, 0.0,0.00,0,0.99, 0.0,0.00,0,0.29
+0.0,0.00,0,0.79, 0.0,0.00,0,0.79, 0.0,0.00,0,0.99
+0.0,0.00,0,0.79, 0.0,0.00,0,0.79, 0.0,0.00,0,0.99
+0.0,0.00,0,0.49, 0.0,0.00,0,0.89, 0.0,0.00,0,0.99
+0.0,0.00,0,0.39, 0.0,0.00,0,0.79, 0.0,0.00,0,0.99
+0.0,0.00,0,0.99, 0.0,0.00,0,0.29, 0.0,0.00,0,0.99
+0.0,0.00,0,0.99, 0.0,0.00,0,0.49, 0.0,0.00,0,0.49
+.2,.2,.2,.2,.2,.0,.0,.0                                      #colors for clipped atoms (rarely used)
+.2,.2,.2,.2,.2,.0,.0,.0
+.2,.2,.2,.2,.2,.0,.0,.0
+.2,.2,.2,.2,.2,.0,.0,.0
+.2,.2,.2,.2,.2,.9,.9,.0
+.2,.2,.2,.2,.2,.0,.0,.0
+255,255,255,255,255,255,1.,1.0                               # background rgb (0-255), fog rgb, fraction fog front and back
+0,0.7,1.1                                                    # cast shadow parameters
+1,0.0023,2.0,1.                                              # fake ambient occlusion parameters
+0.,0                                                         # rotation for stereo pairs
+-30,-30                                                      # image size in pixels, negative numbers pad the molecule by that amount
+illustrate
+3.,10.,3.,8.,4,0.,5.                                         # parameters for outlines, atomic
+3.,10.                                                       # subunits
+3.,8.,6.                                                     # outlines defining regions of the chain
+calculate
+1,0                                                          # first "1" specifies pnm outpu  
+"""
+    astr+=pdbId+".pnm\n"  
+    return astr
+
+#https://files.rcsb.org/download/1crn.pdb
 def processForm(form, verbose = 0):
     if not form:
         displayForm()
         return
+
+    #print "<html>Hello World</html>"
     x = processObj(argsDict)
     x.formParse(form,argsDict, verbose)
-    id=mkRand()
-    param="/home/bioserv/autin/public_html/cgi-bin/param.pmg"
-    wrkDir = "/data/www/htdocs/tmp/PMG/"+id
-    os.mkdir(wrkDir)
-    tmpPDBName = wrkDir+"/ABCgen_0.pdb"
-    httpfile="http://bioserv.rpbs.jussieu.fr/tmp/PMG/"+id+"/ABCgen_0.pdb"
-    httpimg="http://bioserv.rpbs.jussieu.fr/tmp/PMG/"+id+"/PMG_0.png"
     queryTXT = string.upper(x.data["TXT"])
-    pdbout=word(queryTXT)
-    PDBout(pdbout,tmpPDBName)
-    #aStr = waitingPage()
-    #print aStr
-    cmd=("/home/bioserv/autin/public_html/cgi-bin/PMG "+param+" "+wrkDir+" "+tmpPDBName+"\n")
+    id=mkRand()
+    #prepare input
+    redirectURL = "https://mesoscope.scripps.edu/data/tmp/ILL/"+id+"/illustrator.html"
+    wrkDir = "/var/www/html/data/tmp/ILL/"+id
+    print "Content-type: text/html"
+    print
+    #print "<html>Hello World</html>"
+    curentD = os.path.abspath(os.curdir)
+    os.mkdir(wrkDir)
+    inpstring = prepareInput(queryTXT)
+    inpfile = wrkDir+"/"+queryTXT+".inp"
+    f = open(inpfile, "w")
+    f.write(inpstring)
+    f.close()
+    
+    tmpPDBName = wrkDir+"/"+queryTXT+".pdb"
+    cmd = "cd "+wrkDir+";"
+    cmd+= "wget https://files.rcsb.org/download/"+queryTXT+".pdb >/dev/null;"
+    cmd+= curentD+"/illustrator-2016 < "+queryTXT+".inp>/dev/null;"
+    cmd+="/bin/convert "+queryTXT+".pnm -transparent \"rgb(254,254,254)\" "+queryTXT+".png>/dev/null;"
     os.system(cmd)
+    
+    httpfile="https://mesoscope.scripps.edu/data/tmp/ILL/"+id+"/"+queryTXT+".pdb"
+    httpimg="https://mesoscope.scripps.edu/data/tmp/ILL/"+id+"/"+queryTXT+".png"
+    
     aStr = displayResult(tmpPDBName,httpfile,httpimg,queryTXT)
-    f = open(wrkDir+"/ABCgen.html","w")
+    #print aStr
+    f = open(wrkDir+"/illustrator.html","w")
     f.write("%s" % aStr)
     f.close()
-    sys.stdout.write("%s" % htmlRedirectToPage("http://bioserv.rpbs.jussieu.fr/tmp/PMG/"+id+"/ABCgen.html"))
-    sys.stdout.flush()
+    #sys.stdout.write("%s" % htmlRedirectToPage("https://mesoscope.scripps.edu/data/tmp/ILL/"+id+"/illustrator.html"))
+    #sys.stdout.flush()
+
+    
+    #print 'Content-Type: text/html\r\n'
+    print ''# HTTP says you have to have a blank line between headers and content
+    print '<html>\r\n'
+    print '  <head>\r\n'
+    print '    <meta http-equiv="refresh" content="0;url=%s" />\r\n' % redirectURL
+    print '    <title>You are going to be redirected</title>\r\n'
+    print '  </head>\r\n' 
+    print '  <body>\r\n'
+    print '    Redirecting... <a href="%s">Click here if you are not redirected</a>\r\n' % redirectURL
+    print '  </body>\r\n'
+    print '</html>\r\n'
+
+  
     #displayResult(tmpPDBName,httpfile,httpimg,queryTXT)
     cleanup(wrkDir, "5 days")
     return
@@ -174,39 +281,13 @@ def processForm(form, verbose = 0):
 def displayResult(fName,httpn,htti,valStr):
     suffix="0"
     jSize="500"
-
-    aStr=""
-    aStr += """
-    <html>
-    <head>
-    <title>ABCgen - Results</title>
-    <meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />
-    <link type=\"text/css\" rel=\"StyleSheet\" href=\"http://bioserv.rpbs.jussieu.fr/tools/slider/css/luna2/luna.css\" />
-    <script src=\"http://bioserv.rpbs.jussieu.fr/tools/jmol-11.0.RC3/Jmol.js\"></script>  <!-- REQUIRED -->
-    </head>
-    """
-
-    aStr += "<body bgcolor=\"white\">\n"
-    aStr += "<div align=\"center\">\n"
-    #aStr += jmolInit()
-    aStr +="<script>\njmolInitialize(\"../../../tools/jmol-11.0.RC3/\");\n"
-    aStr +="jmolSetAppletColor(\"black\"); // if you don't want black change it \n</script>\n"
-
-    aStr += "<div align=\"center\"><img src=\"http://bioserv.rpbs.jussieu.fr/~autin/help/ABCgen/ABCgen.png\" width=\"800\" height=\"89\"></a></div>\n"
-    #aStr += htmlH4Msg("Alphabet structure generator")
+    aStr = ""
+    aStr += htmlHeader("Illustrator",httpHead = 0,other = "<META NAME=\"keywords\" content=\"\">")
+    #aStr += "<div align=\"center\"><img src=\"http://bioserv.rpbs.jussieu.fr/~autin/help/ABCgen/ABCgen.png\" width=\"800\" height=\"89\"></a></div>\n"
+    aStr += htmlRule()
     aStr += htmlH4Msg("Query text :"+valStr)
     aStr += "Download the PDB coordinate file <a href=\""+httpn+"\">HERE</a><br>\n"
-    #aStr += "Once you have downloaded the PDB file, feel free to render it using our <a href=\"http://bioserv.rpbs.jussieu.fr/~autin/cgi-bin/PMG\" target=\"blanck\">Protein Movie Generator web service</a><br>\n"
-    aStr += "Made with JMol and PMG  <a href=\"http://bioserv.rpbs.jussieu.fr/~autin/cgi-bin/PMG\" target=\"blanck\">Protein Movie Generator web service</a><br>\n"
-    aStr += "<table><tr>\n"
-    aStr += "<td>\n"
-    aStr += "<script>\n"
-    aStr +="jmolApplet(%d, \"load %s; cartoon off;wireframe off; cpk on;rotate x 0; rotate y 0; rotate z 0\", %s);\n" % (int(jSize),httpn, suffix)
-    aStr +="</script>\n"
-    aStr +="</td><td>\n"
     aStr +="<a href=\""+htti+"\" target=\"blanck\"><img src=\""+htti+"\" width="+jSize+" height="+jSize+"></a>\n"
-    aStr +="</td></tr></table>\n"
-    aStr +="</div>\n"
     aStr += htmlTailer()
     return aStr
 
